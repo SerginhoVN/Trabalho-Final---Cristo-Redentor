83810e8bb4be4ffa3d5ea43cb66fa2eb1c1bf95c
Sérgio Vieira <114782290+SerginhoVN@users.noreply.github.com>
2023-11-21 18:18:48 -0300

    Create tailwind.config.cjs
    Define um arquivo de configuração do Tailwind CSS

---
+++ tailwind.config.cjs
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      backgroundImage: {
+        // 'cristo-wp': 'url(/cristo-wp.jpg)',
+        'cristo-wp': 'linear-gradient( rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5) ), url("./assets/cristo-wp.jpg")',
+        'animation-wp': 'linear-gradient( rgba(0, 0, 0, 0.8), rgba(0, 0, 0, 0.8) ), url("./assets/animation-wp.jpeg")',
+        'rio-wp': 'linear-gradient( rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5) ), url("./assets/rio.jpeg")',
+      },
+    },
+  },
+  plugins: [],
+}
