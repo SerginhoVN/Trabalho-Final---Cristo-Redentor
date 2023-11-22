a0ca4c30ea789d1ac9662f1be070120c3c4feef1
Vinicius Sueiro <hi@vsueiro.com>
2023-11-22 17:58:18 -0500

    Troca nome da pasta que estava hard-coded

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -5,11 +5,11 @@ export default {
     extend: {
       backgroundImage: {
         // 'cristo-wp': 'url(/cristo-wp.jpg)',
-        'cristo-wp': 'linear-gradient( rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5) ), url("./Source Code/Assets
+        'cristo-wp': 'linear-gradient( rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5) ), url("./src/Assets
 /cristo-wp.jpg")',
-        'animation-wp': 'linear-gradient( rgba(0, 0, 0, 0.8), rgba(0, 0, 0, 0.8) ), url("./Source Code/Assets
+        'animation-wp': 'linear-gradient( rgba(0, 0, 0, 0.8), rgba(0, 0, 0, 0.8) ), url("./src/Assets
 /animation-wp.jpeg")',
-        'rio-wp': 'linear-gradient( rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5) ), url("./Source Code/Assets
+        'rio-wp': 'linear-gradient( rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5) ), url("./src/Assets
 /rio.jpeg")',
       },
     },
