8fcc30d5f0bb2bb321347c6479bbbb4a171bb6fd
Sérgio Vieira <114782290+SerginhoVN@users.noreply.github.com>
2023-11-22 00:27:36 -0300

    tailwind.config.cjs

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -5,9 +5,12 @@ export default {
     extend: {
       backgroundImage: {
         // 'cristo-wp': 'url(/cristo-wp.jpg)',
-        'cristo-wp': 'linear-gradient( rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5) ), url("./assets/cristo-wp.jpg")',
-        'animation-wp': 'linear-gradient( rgba(0, 0, 0, 0.8), rgba(0, 0, 0, 0.8) ), url("./assets/animation-wp.jpeg")',
-        'rio-wp': 'linear-gradient( rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5) ), url("./assets/rio.jpeg")',
+        'cristo-wp': 'linear-gradient( rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5) ), url("./Source Code/Assets
+/cristo-wp.jpg")',
+        'animation-wp': 'linear-gradient( rgba(0, 0, 0, 0.8), rgba(0, 0, 0, 0.8) ), url("./Source Code/Assets
+/animation-wp.jpeg")',
+        'rio-wp': 'linear-gradient( rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5) ), url("./Source Code/Assets
+/rio.jpeg")',
       },
     },
   },
