22a07719cdbf9e40dda05edd3a4d23663f8ec3cd
Sérgio Vieira <serginho.vieira.rio@gmail.com>
2023-12-07 16:45:42 -0300

    correção caminho

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -6,11 +6,11 @@ export default {
       backgroundImage: {
         // 'cristo-wp': 'url(/cristo-wp.jpg)',
         "cristo-wp":
-          'linear-gradient( rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5) ), url("./src/assets/cristo-wp.jpg")',
+          'linear-gradient( rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5) ), url("./assets/src/assets/cristo-wp.jpg")',
         "animation-wp":
-          'linear-gradient( rgba(0, 0, 0, 0.8), rgba(0, 0, 0, 0.8) ), url("./src/assets/animation-wp.jpeg")',
+          'linear-gradient( rgba(0, 0, 0, 0.8), rgba(0, 0, 0, 0.8) ), url("./assets/src/assets/animation-wp.jpeg")',
         "rio-wp":
-          'linear-gradient( rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5) ), url("./src/assets/rio.jpeg")',
+          'linear-gradient( rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5) ), url("./assets/src/assets/rio.jpeg")',
       },
     },
   },
